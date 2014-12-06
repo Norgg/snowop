d7c76e33b689d3f39032386b2e65a3f219258db5
John Turner <groups@norgg.org>
2014-12-06 16:38:47 +0000

    Joystick config for firing.

--- Assets/Controls.cs
+++ Assets/Controls.cs
@@ -3,10 +3,12 @@ using System.Collections;
 
 public class Controls : MonoBehaviour {
 
-	public string horizontal = "Horizontal1";
-	public string vertical = "Vertical1";
-	public string fire = "Fire1";
+	public string horizontal;
+	public string vertical;
+	public string fire;
 	public float speed = 0.1f;
+	public int fireTime = 30;
+	int fireTimer = 0;
 	public GameObject snowball;
 	Terrain terrain;
 
@@ -29,6 +31,8 @@ public class Controls : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		if (fireTimer > 0) fireTimer--;
+
 		Vector3 vel = new Vector3(Input.GetAxis(horizontal), 0f, Input.GetAxis(vertical));
 
 		if (vel.magnitude > 1f) {
@@ -44,6 +48,14 @@ public class Controls : MonoBehaviour {
 			transform.position += vel;
 		} else {
 			rigidbody.angularVelocity *= 0.2f;
+		}
+
+		// Fire
+		if (Input.GetAxis(fire) < 0 && fireTimer == 0) {
+			fireTimer = fireTime;
+			GameObject newSnowball = (GameObject)Object.Instantiate(snowball);
+			newSnowball.transform.position = transform.position + transform.forward * 0.5f + new Vector3(0, 1, 0);
+			newSnowball.rigidbody.velocity = 20 * transform.forward + new Vector3(0, 1, 0);
 		}
 
 		if (Input.GetKeyDown(KeyCode.Escape)) {
