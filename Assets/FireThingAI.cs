8090aad5643f38b0dc7cd0dae15065d3ead87c33
John Turner <groups@norgg.org>
2014-12-07 23:50:15 +0000

    Animated fire texture. Music tweaks.

--- Assets/FireThingAI.cs
+++ Assets/FireThingAI.cs
@@ -35,6 +35,7 @@ public class FireThingAI : MonoBehaviour {
 
 	// Update is called once per frame
 	void FixedUpdate () {
+		renderer.material.mainTextureOffset += new Vector2(Random.value/100f, Random.value/100f);
 		rigidbody.angularVelocity = new Vector3(0, 25, 0);
 
 		Vector3 pos = transform.position;
